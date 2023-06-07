418326533bc6a76ead4a61cf0ae124f1439f244d
Hon Chow <honfungchow@gmail.com>
2023-06-08 01:56:34 +0800

    first commit

---
+++ utils/data.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np 
+from sklearn.model_selection import train_test_split
+
+RAW_PATH = "../data/sudoku_pq/sudoku.parquet"
+
+def read_data(path):
+
+    df = pd.read_parquet(path)
+
+    for col in df:
+        for row in range(len(df)):
+            df[col][row] = np.array([np.uint(i) for i in df[col][row]])
+
+    return df
+
+def _train_test_split(data, test_ratio = 0.2):
+
+    X = data["puzzle"]
+    y = data["solution"]
+
+    X_train, X_test, y_train, y_test = _train_test_split(X, y, 
+                                                        test_size=test_ratio, 
+                                                        random_state=64)
+    return X_train, X_test, y_train, y_test
+
+def fetch_data(path=RAW_PATH):
+
+    print("Reading and converting data...")
+    data = read_data(path)
+    print("Reading complete")
+
+    X_train, X_test, y_train, y_test = _train_test_split(data)
+
+    return X_train, X_test, y_train, y_test
+
+X_train, X_test, y_train, y_test = fetch_data()
+
