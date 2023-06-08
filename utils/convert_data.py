9dc36a6fdb0fa4446c1414d203e7c47f8504d078
Hon Chow <honfungchow@gmail.com>
2023-06-09 01:17:14 +0800

    updated with conversion to nparray

--- utils/convert_data.py
+++ utils/convert_data.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np 
 from sklearn.model_selection import train_test_split
 
-RAW_PATH = "../data/sudoku_pq/sudoku.parquet"
-TRAIN_PATH = "../data/sudoku_pq_train/sudoku_pq_train.parquet"
-TEST_PATH = "../data/sudoku_pq_test/sudoku_pq_test.parquet"
-
+RAW_PATH = "../data/sudoku/sudoku_pq/sudoku.parquet"
+TRAIN_DATA_PATH = "../data/sudoku_pq/sudoku_pq_train_data.parquet"
+TRAIN_LABEL_PATH = "../data/sudoku_pq/sudoku_pq_train_label.parquet"
+TEST_DATA_PATH = "../data/sudoku_pq/sudoku_pq_test_data.parquet"
+TEST_LABEL_PATH = "../data/sudoku_pq/sudoku_pq_test_label.parquet"
 
 def data_trans(df):
     return np.array([np.uint(i) for i in df])
@@ -37,16 +38,24 @@ def fetch_data(path=RAW_PATH):
 
     return X_train, X_test, y_train, y_test
 
-def convert_to_nparray(src=RAW_PATH, train_dst=TRAIN_PATH, test_dst=TEST_PATH):
+def convert_to_nparray(src=RAW_PATH, train_data_dst=TRAIN_DATA_PATH, 
+                       train_label_dst=TRAIN_LABEL_PATH,
+                       test_data_dst=TEST_DATA_PATH,
+                       test_label_dst=TEST_LABEL_PATH):
 
     print("Saving results as parquet file")
 
     X_train, X_test, y_train, y_test = fetch_data(src)
 
-    df_train = pd.DataFrame({"puzzle": X_train, "solution": y_train})
-    df_test = pd.DataFrame({"puzzle": X_test, "solution": y_test})
+    df_train_data = pd.DataFrame({"puzzle": X_train})
+    df_train_label = pd.DataFrame({"solution": y_train})
+    df_test_data = pd.DataFrame({"puzzle": X_test})
+    df_test_label = pd.DataFrame({"solution": y_test})
 
-    df_train.to_parquet(train_dst)
-    df_test.to_parquet(test_dst)
+    df_train_data.to_parquet(train_data_dst)
+    df_train_label.to_parquet(train_label_dst)
+    df_test_data.to_parquet(test_data_dst)
+    df_test_label.to_parquet(test_label_dst)
 
 convert_to_nparray()
+
